4ac3015600ea5d2e8c78ecf972882564b7d8826d
Administrator <Administrator@SC-201704091246>
2018-01-28 02:00:54 +0800

    ww

--- src/ccu/campus/association/service/UserService.java
+++ src/ccu/campus/association/service/UserService.java
@@ -6,6 +6,7 @@ import ccu.campus.association.pojo.User;
 
 public interface UserService {
 	 void addUser(String username,String password);
+	 
 	 void updateUser(User User);
 	 void deleteUserById(User User);
 	 User queryUserById(String id);
